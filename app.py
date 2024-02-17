51db42a83dbba265c4b8ccccb9b6db7873b3bfb5
bago <bahaddin.ahmadov@limestonedigital.com>
2024-02-17 15:37:17 +0100

    clear result and cache added

--- app.py
+++ app.py
@@ -4,28 +4,30 @@ import os
 import random
 from pymongo import MongoClient
 import pandas as pd
+import json
 from database import db
+from db import load_data
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
 verbs_collection = db['verbs']
-
+used_verb_ids = []
 
 @app.route('/')
 def index():
-    used_verb_ids = session.get('used_verb_ids', [])
+    global used_verb_ids
     print("used_verb_ids size: ", len(used_verb_ids))
     verbs = list(verbs_collection.find({'_id': {'$nin': used_verb_ids}}))
     if verbs:
         cur_verb = random.choice(verbs)
-        cur_verb['_id'] = str(cur_verb['_id'])  # Ensure '_id' is set as a string
+        cur_verb['_id'] = str(cur_verb['_id'])
         random_conjugation = random.choice(cur_verb['conjugations'])
 
         session['current_verb'] = {
-            '_id': cur_verb['_id'],  # Ensure '_id' is set in the session
+            '_id': cur_verb['_id'],
             'infinitive_eng': cur_verb['infinitive_eng'],
-            'conjugation_pol': random_conjugation['conjugation_pol']  # Store correct Polish form for answer validation
+            'conjugation_pol': random_conjugation['conjugation_pol']
         }
 
         question = f"Type the Polish form of '{random_conjugation['pronoun_eng']} {random_conjugation['conjugation_eng']}'"
@@ -36,7 +38,6 @@ def index():
     else:
         return render_template('random_verb.html',
                                feedback="No verbs found in the database. Please upload a file to add verbs.")
-
 
 @app.route('/submit', methods=['POST'])
 def submit_answer():
@@ -50,14 +51,25 @@ def submit_answer():
     if user_answer.lower() == correct_answer.lower():
         session['feedback'] = "Correct!"
         session['score'] = session.get('score', 0) + 1
-        used_verb_ids = session.get('used_verb_ids', [])
         used_verb_ids.append(session['current_verb']['_id'])
-        session['used_verb_ids'] = used_verb_ids
     else:
         session['feedback'] = f"Incorrect. The correct answer is {correct_answer}."
 
     return redirect(url_for('index'))
 
+@app.route('/clear_results', methods=['POST'])
+def clear_results():
+    session.pop('score', None)
+    session.pop('used_verb_ids', None)
+    return redirect(url_for('index'))
+
+@app.route('/clear_cache', methods=['POST'])
+def clear_cache():
+    global cached_data
+    cached_data = None
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
+    file_path = 'upload.json'
+    load_data(file_path)
     app.run(debug=True)
--- db.py
+++ db.py
@@ -1,19 +1,15 @@
 import json
-# Import database configuration from database.py
 from database import db
 
 verbs_collection = db['verbs']
-
-verbs_collection = db['verbs']
-cached_data = None  # Global variable to store loaded data
+cached_data = None
 
 def load_data(file_path):
-    global cached_data  # Access the global variable
-    if cached_data is None:  # Load data only if it's not cached
+    global cached_data
+    if cached_data is None:
         with open(file_path, 'r', encoding='utf-8') as file:
             cached_data = json.load(file)
     return cached_data
-
 
 def insert_verbs(data):
     result = verbs_collection.insert_many(data)
