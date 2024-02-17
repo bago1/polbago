d345f288ee1f57e1651a546e9d3759548fde651e
bago <bahaddin.ahmadov@limestonedigital.com>
2024-02-17 15:15:35 +0100

    cached

--- db.py
+++ db.py
@@ -4,10 +4,16 @@ from database import db
 
 verbs_collection = db['verbs']
 
+verbs_collection = db['verbs']
+cached_data = None  # Global variable to store loaded data
+
 def load_data(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        data = json.load(file)
-    return data
+    global cached_data  # Access the global variable
+    if cached_data is None:  # Load data only if it's not cached
+        with open(file_path, 'r', encoding='utf-8') as file:
+            cached_data = json.load(file)
+    return cached_data
+
 
 def insert_verbs(data):
     result = verbs_collection.insert_many(data)
