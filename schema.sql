0f75241a9b6866916db5e4e9abf9308b878023cc
bago <bahaddin.ahmadov@limestonedigital.com>
2024-02-17 06:45:45 +0100

    main.py deleted

---
+++ schema.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS verbs;
+CREATE TABLE verbs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    infinitive TEXT NOT NULL,
+    english_1 TEXT,
+    polish_1 TEXT,
+    english_2 TEXT,
+    polish_2 TEXT,
+    english_3 TEXT,
+    polish_3 TEXT,
+    english_4 TEXT,
+    polish_4 TEXT,
+    english_5 TEXT,
+    polish_5 TEXT,
+    english_6 TEXT,
+    polish_6 TEXT
+);
