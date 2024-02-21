9cce8aaf024fd003c1f65184598d27b65568277b
bago1 <ahmadovba1@gmail.com>
2024-02-21 11:40:15 +0100

    seperated load data vs fetch

--- db.py
+++ db.py
@@ -9,7 +9,7 @@ cached_data = None
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def load_data():
+def fetch_data_from_mongo():
     global cached_data
     if cached_data is None:
         logger.info("Fetching data from MongoDB...")
@@ -25,55 +25,35 @@ def insert_verbs(data):
     result = verbs_collection.insert_many(data)
     logger.info(f"{len(result.inserted_ids)} documents inserted.")
 
+def upload_json_data(file_path):
+    """
+    Load data from a JSON file.
+
+    :param file_path: Path to the JSON file.
+    :return: Data loaded from the JSON file.
+    """
+    global cached_data
+    if cached_data is None:
+        try:
+            with open(file_path, 'r', encoding='utf-8') as file:
+                cached_data = json.load(file)
+                logger.info(f"Data loaded successfully from {file_path}.")
+        except FileNotFoundError:
+            logger.error(f"File not found: {file_path}")
+            cached_data = []
+        except json.JSONDecodeError:
+            logger.error(f"Error decoding JSON from file: {file_path}")
+            cached_data = []
+    return cached_data
+
 if __name__ == "__main__":
     file_path = 'upload2.json'
-    data = load_data(file_path)
+    data = upload_json_data(file_path)
     insert_verbs(data)
 
 
-# //import json
-# import logging
-# from database import db
-#
-# verbs_collection = db['verbs2']
-# cached_data = None
-#
-# # Configure logging
-# logging.basicConfig(level=logging.INFO)
-# logger = logging.getLogger(__name__)
-#
-#
-# def load_data(file_path):
-#     """
-#     Load data from a JSON file.
-#
-#     :param file_path: Path to the JSON file.
-#     :return: Data loaded from the JSON file.
-#     """
-#     global cached_data
-#     if cached_data is None:
-#         try:
-#             with open(file_path, 'r', encoding='utf-8') as file:
-#                 cached_data = json.load(file)
-#                 logger.info(f"Data loaded successfully from {file_path}.")
-#         except FileNotFoundError:
-#             logger.error(f"File not found: {file_path}")
-#             cached_data = []
-#         except json.JSONDecodeError:
-#             logger.error(f"Error decoding JSON from file: {file_path}")
-#             cached_data = []
-#     return cached_data
-#
-#
-#
-# def insert_verbs(data):
-#     result = verbs_collection.insert_many(data)
-#     logger.info(f"{len(result.inserted_ids)} documents inserted.")
-#
-# if __name__ == "__main__":
-#     file_path = 'upload2.json'
-#     data = load_data(file_path)
-#     insert_verbs(data)
 
 
-# burda 2 load data var 1-si fayldan cekmek ucun (mongoya basmaga) diger app-in ozunun mongodan cekmeis onlari ayir
+
+
+
